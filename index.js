a0d87dd6c86db5041153e235d86f8549d6c5dcf8
saitama-on <yashjoshi6787@gmail.com>
2024-03-11 17:44:23 +0530

    new+ commit

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let config = document.getElementById('configurator');
 
 config.addEventListener('click' , ()=>{
 
-    window.location.replace("http://127.0.0.1:5500/yousolar/calc.html")
+    window.location.replace("calc.html")
 })
 
 
